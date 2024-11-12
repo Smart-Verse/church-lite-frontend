ef46f7743d2a75f86db57b97d7e6ec16383ec22e
Geovane Araujo <geovane-araujo@outlook.com>
2024-11-12 08:04:07 -0300

    Criação do arquivo de configuração

--- src/app/components/datatable/datatable.ts
+++ src/app/components/datatable/datatable.ts
@@ -2,10 +2,17 @@ export class DataTable {
     values: any[] = [];
     fields: Column[] = [];
     route: string = "";
-    classBase?: Function
+    classBase?: Function;
+    filters?: Filters
 }
+
 export class Column {
     field: string = "";
     header: string = "";
     width: string = "";
+}
+
+export class Filters {
+  field: string = "";
+  type: string = "";
 }
